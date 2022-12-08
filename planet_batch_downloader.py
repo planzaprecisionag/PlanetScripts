a4a4f1809a91051281504fb31704f8faf763373f
planza <planzaprecisionag@gmail.com>
2022-12-08 10:38:24 -0500

    check for file.exists; added overwrite flag

---
+++ planet_batch_downloader.py
@@ -0,0 +1,7 @@
+#%% Library imports
+from planet_raster_downloader import get_planet_download_stats, download_planet_rasters
+
+#%% Getting param values to pass to stats and dl fxns
+download_root_dir = r''
+
+download_planet_rasters()
